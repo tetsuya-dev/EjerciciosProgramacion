df786d58693908bcdb0c91aa677c216415ecc287
Raul Sanchez <rsanchelop2@educacion.navarra.es>
2025-12-01 12:47:55 +0100

    Coreccion nombre paquete Buscaminas

--- src/es/masanz/ut5/ejercicios/EjerExtra1Bsucaminas/Main.java
+++
@@ -1,9 +0,0 @@
-package es.masanz.ut5.ejercicios.EjerExtra1Bsucaminas;
-
-public class Main {
-    public static void main(String[] args) {
-        BuscaMinas buscaMinas = new BuscaMinas(10,10,10);
-
-        buscaMinas.mostrarBuscaminas();
-    }
-}
