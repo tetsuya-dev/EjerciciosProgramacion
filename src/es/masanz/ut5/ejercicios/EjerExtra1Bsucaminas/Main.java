5e6c8ad543865005a054529117f2f32e7c6910bc
Raul Sanchez <rsanchelop2@educacion.navarra.es>
2025-12-01 12:43:38 +0100

    Base incompleta buscaminas

---
+++ src/es/masanz/ut5/ejercicios/EjerExtra1Bsucaminas/Main.java
@@ -0,0 +1,9 @@
+package es.masanz.ut5.ejercicios.EjerExtra1Bsucaminas;
+
+public class Main {
+    public static void main(String[] args) {
+        BuscaMinas buscaMinas = new BuscaMinas(10,10,10);
+
+        buscaMinas.mostrarBuscaminas();
+    }
+}
